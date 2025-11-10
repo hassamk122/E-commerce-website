610105a1906725b8937e84e4ae2eba012d6ab9c8
hassamk122 <hassamkiani66@gmail.com>
2025-11-10 11:43:12 +0500

    :)

--- src/main/java/com/bsse5a/EcommerceWeb/controllers/HomeController.java
+++ src/main/java/com/bsse5a/EcommerceWeb/controllers/HomeController.java
@@ -1,7 +1,9 @@
 package com.bsse5a.EcommerceWeb.controllers;
 
 import com.bsse5a.EcommerceWeb.Security.CurrentUserDetails;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,12 @@ public class HomeController {
 
     @GetMapping("/")
     public String pageOnLoad(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null && auth.isAuthenticated()
+                && !(auth.getPrincipal() instanceof String && auth.getPrincipal().equals("anonymousUser"))) {
+            return "redirect:/home";
+        }
         return "index";
     }
 
